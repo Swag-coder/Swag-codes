c1d361fd1ae0967a5321f07b4101c7bec2dc2ea5
Swag-coder <109068446+Swag-coder@users.noreply.github.com>
2022-07-17 14:49:36 +0530

    Update Gregorian Calender.py

--- Gregorian Calender.py
+++ Gregorian Calender.py
@@ -6,57 +6,57 @@ def isleapyear(x):
     else:
         return False
 
-date=input("Enter the date in dd/mm/yyyy format: ")
-ref="01/01/1988"
-yr=int(date[6:])
-yrgap=yr-1988
-leapyrs=0
-if yr>=1988:
-    for i in range(1988,yr):
+date = input("Enter the date in dd/mm/yyyy format: ")
+ref = "01/01/1988"
+yr = int(date[6:])
+yrgap = yr - 1988
+leapyrs = 0
+if yr >= 1988:
+    for i in range(1988, yr):
         if isleapyear(i):
-            leapyrs+=1
+            leapyrs += 1
 else:
-    for i in range(1987,yr-1,-1):
+    for i in range(1987, yr-1, -1):
         if isleapyear(i):
-            leapyrs+=1
-if yr>=1988:
-    yrdays=yrgap*365+leapyrs
+            leapyrs += 1
+if yr >= 1988:
+    yrdays = yrgap * 365 + leapyrs
 else:
-    yrdays=yrgap*365-leapyrs
+    yrdays = yrgap * 365 - leapyrs
 
-mnthdays=0
-for i in range(1,int(date[3:5])):
-    if i==1:
-        mnthdays+=30
-    elif i==2:
+mnthdays = 0
+for i in range(1, int(date[3:5])):
+    if i == 1:
+        mnthdays += 30
+    elif i == 2:
         if isleapyear(yr):
-            mnthdays+=29
+            mnthdays += 29
         else:
-            mnthdays+=28
-    elif i==3:
-        mnthdays+=31
-    elif i==4:
-        mnthdays+=30
-    elif i==5:
-        mnthdays+=31
-    elif i==6:
-        mnthdays+=30
-    elif i==7:
-        mnthdays+=31
-    elif i==8:
-        mnthdays+=31
-    elif i==9:
-        mnthdays+=30
-    elif i==10:
-        mnthdays+=31
-    elif i==11:
-        mnthdays+=30
+            mnthdays += 28
+    elif i == 3:
+        mnthdays += 31
+    elif i == 4:
+        mnthdays += 30
+    elif i == 5:
+        mnthdays += 31
+    elif i == 6:
+        mnthdays += 30
+    elif i == 7:
+        mnthdays += 31
+    elif i == 8:
+        mnthdays += 31
+    elif i == 9:
+        mnthdays += 30
+    elif i == 10:
+        mnthdays += 31
+    elif i == 11:
+        mnthdays += 30
 
-datedays=int(date[:2]) if int(date[3:5])!=1 else int(date[:2])-1
-days=yrdays+mnthdays+datedays
-if yr>=1988:
-    rem=days%7
+datedays = int(date[:2]) if int(date[3:5]) != 1 else int(date[:2]) - 1
+days = yrdays + mnthdays + datedays
+if yr >= 1988:
+    rem = days % 7
 else:
-    rem=days%-7
-week=["Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday"]
+    rem = days % -7
+week = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
 print(week[rem])
