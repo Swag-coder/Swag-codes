1df42cc28bdda9d01b3d3c3d8f93edc255ff3797
Swag-coder <109068446+Swag-coder@users.noreply.github.com>
2022-07-24 12:48:28 +0530

    Add files via upload

---
+++ Circle Sequence.py
@@ -0,0 +1,41 @@
+import math
+
+n = int(input("Enter the number: "))
+sqr = []
+for num in range(1, int(math.sqrt(2 * n)) + 1):
+    sqr.append(num ** 2)
+    
+seq = [1] + [2] * (n - 1)
+while seq != [1] + [n] * (n - 1):
+    for i in range(n):
+        for j in range(i + 1, n):
+            if seq[i] == seq[j]:
+                break
+        else:
+            continue
+        break
+
+    else:
+        for i in range(n):
+            if seq[i] + seq[(i + 1) % n] not in sqr:
+                break
+        else:
+            print(*seq)
+
+    if seq[-1] == n:
+        seq[-1] = 2
+        inc = 'yes'
+    else:
+        seq[-1] += 1
+        inc = 'no'
+    for i in range(n - 2, 0, -1):
+        if inc == 'yes':
+            if seq[i] == n:
+                seq[i] = 2
+                inc = 'yes'
+            else:
+                seq[i] += 1
+                inc = 'no'
+
+    
+    
