46579b9804b3ebfc5a40c886988117b418deec4c
Swag-coder <109068446+Swag-coder@users.noreply.github.com>
2022-08-01 14:54:24 +0530

    Add files via upload

---
+++ Circle Sequence 3.py
@@ -0,0 +1,44 @@
+import time
+import math
+
+sqr = []
+n = int(input('Enter the number: '))
+start = time.monotonic()
+for i in range(2, int(math.sqrt(n * 2)) + 1):
+    sqr.append(i ** 2)
+    
+lst = list(range(2, n + 1))
+seq = [1]
+ind = 0
+while len(lst) != 0 and len(lst) != n:
+    for i in range(ind, len(lst)):
+        if lst[i] + seq[-1] in sqr:
+            if len(lst) != 1:
+                seq.append(lst[i])
+                del lst[i]
+                ind = 0
+                break
+            else:
+                if lst[i] + 1 in sqr:
+                    seq.append(lst[i])
+                    del lst[i]
+                    ind = 0
+                    break
+                else:
+                    continue
+                    
+    else:
+        lst.append(seq[-1])
+        lst.sort()
+        ind = lst.index(seq[-1]) + 1
+        del seq[-1]
+else:
+    t = time.monotonic() - start
+    if len(lst) == n:
+        print("No circle exists for the number", n)
+    else:
+        print(*seq)
+    print("Time taken: {}s".format(t))
+        
+            
+           
